c425e734d6649dc61ae261861914f85d5799795f
ptamnguy <phuong.tam_nguyen.ext@nokia.com>
2026-01-15 10:14:48 +0530

    FEAT: event publish from service-a

--- service-a/src/main/java/com/dthvinh/dto/UpdatePersonDto.java
+++ service-a/src/main/java/com/dthvinh/dto/UpdatePersonDto.java
@@ -1,9 +1,5 @@
 package com.dthvinh.dto;
 
-import java.util.UUID;
+public record UpdatePersonDto(String name, int age) {
 
-public record UpdatePersonDto(UUID id, String name, int age) {
-    public UUID getId() {
-        return id;
-    }
 }
